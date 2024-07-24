6a08b1b68f6e122a2491a5f3c2d8cd780154a94d
Ran Lachmy <ranlachmy96@gmail.com>
2024-07-24 15:38:15 +0300

    added comments to the anomely detection section

--- anamolyDetector/anamolyDetector.py
+++ anamolyDetector/anamolyDetector.py
@@ -7,15 +7,23 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 import joblib
 
-# Load the dataset
+# **************************************************************************************
+# Load and preprocess dataset from ARFF file.
+# **************************************************************************************
 file = open("final-dataset.arff")
 decoder = arff.ArffDecoder()
 data = decoder.decode(file, encode_nominal=True)
 print(data['data'][2])
 
+# **************************************************************************************
+# Extract features and labels from the dataset
+# **************************************************************************************
 vals = [val[0:-1] for val in data['data']]
 labels = [lab[-1] for lab in data['data']]
 
+# **************************************************************************************
+# Data balancing: ensure each class has a sufficient number of samples.
+# **************************************************************************************
 da = set(labels)
 brac = 600
 templ = []
@@ -36,21 +44,32 @@ labels = templ
 l = len(vals)
 print(l)
 
+# **************************************************************************************
+# Split the dataset into training and testing sets.
+# **************************************************************************************
 X_train, X_test, Y_train, Y_test = train_test_split(vals, labels, stratify=labels,test_size=0.2, random_state=0)
 
+# **************************************************************************************
+# Standardize the features for better model performance.
+# **************************************************************************************
 scaler = StandardScaler()
 x_train = scaler.fit_transform(X_train)
 x_test = scaler.transform(X_test)
 y_train = np.array(Y_train)
 y_test = np.array(Y_test)
 
-# SVM
+# **************************************************************************************
+# Train an SVM model and save it.
+# **************************************************************************************
 model = SVC(kernel='sigmoid', gamma='auto')
 model.fit(x_train, y_train)
 
 joblib.dump(model, 'model.joblib')
 joblib.dump(scaler, 'scaler.joblib')
 
+# **************************************************************************************
+# Evaluate the model's performance on the test set.
+# **************************************************************************************
 y_pred = model.predict(x_test)
 y_pred = pd.DataFrame(y_pred)
 print((accuracy_score(y_test, y_pred))*100, '%')
