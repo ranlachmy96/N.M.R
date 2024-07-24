1ad291336192756dc7314db5e535b776f1e8512d
Ran Lachmy <ranlachmy96@gmail.com>
2024-07-24 16:02:51 +0300

    added comments to the port change section

--- nodeServer/portChanging/portChange.js
+++ nodeServer/portChanging/portChange.js
@@ -1,3 +1,11 @@
+/** *************************************************************************************
+ Importing the `portChanging` module which is expected to contain functionality for 
+ starting a server and managing port changes.
+ Determines the port number to be used for the server. It takes the port number from 
+ the command-line arguments, environment variables, or defaults to 3002 if neither is provided.
+ Starts the server using the `startServer` function from the `portChanging` module and 
+ the determined port number.
+ ************************************************************************************** */
 const portChanging = require('./portChanging')
 const newPort = process.argv[2] || process.env.PORT || 3002
 portChanging.startServer(newPort)
--- nodeServer/portChanging/portChanging.js
+++ nodeServer/portChanging/portChanging.js
@@ -1,6 +1,20 @@
+/** *************************************************************************************
+ Importing the `spawn` function from the `child_process` module to create child processes 
+ and the `path` module to handle file paths.
+ ************************************************************************************** */
 const { spawn } = require('child_process');
 const path = require('path');
 
+/** *************************************************************************************
+ Defines the `startServer` function which starts a server on the specified port.
+ Resolves the absolute path to the 'index.js' file located one directory above 
+ the current directory.
+ Spawns a new Node.js process to run the 'index.js' script with the given port.
+ Handles errors encountered when attempting to start the server, such as port in use.
+ If the port is in use, tries to start the server on the next available port.
+ Logs data received from the server's standard output (stdout).    
+ Logs when the server process exits, along with the exit code.
+ ************************************************************************************** */
 exports.startServer = (port) => {
     const indexPath = path.resolve(__dirname, '../index.js');
     const server = spawn('node', [indexPath, port]);
