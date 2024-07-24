a9664381bac2ecec2a7e5497ce0764b1a945c50e
Ran Lachmy <ranlachmy96@gmail.com>
2024-07-24 19:56:02 +0300

    fixed the port changing

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,18 +1,42 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const app = express();
+const { reportsRouter } = require('./routers/reportRouter');
+const port = process.argv[2] || process.env.PORT || 3000;
 
-const logger = require('morgan') // NOTE: for debugging
-const app = express()
-const port =process.argv[2] || process.env.PORT || 3000
-const { reportsRouter } = require('./routers/reportRouter')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/data', reportsRouter);
+app.use(logger('dev'));
 
+let server;
 
-app.use(express.static(path.join(__dirname, 'public')))
+const startServer = (port) => {
+    server = app.listen(port, () => console.log(`Express server is running on port ${port}`))
+        .on('error', (error) => {
+            if (error.code === 'EADDRINUSE') {
+                console.log(`Port ${port} is already in use. Trying another port...`);
+                shutdown(() => startServer(port + 2)); // Try the next port
+            } else {
+                console.error(`Server error: ${error}`);
+            }
+        });
+};
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/data', reportsRouter)
+// Graceful shutdown
+const shutdown = (callback) => {
+    if (server) {
+        console.log('Shutting down server...');
+        server.close(() => {
+            console.log('Server closed.');
+            if (callback) callback();
+        });
+    }
+};
 
-app.use(logger('dev'))
+process.on('SIGINT', () => shutdown(() => process.exit(0)));
+process.on('SIGTERM', () => shutdown(() => process.exit(0)));
 
-app.listen(port, () => console.log(`Express server is running on port ${port}`))
+startServer(port);
